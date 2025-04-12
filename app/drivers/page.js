49e3906d3caf9eb152cc30e3b880ffd8996511bd
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-04-12 00:44:29 -0600

    fixed data fetching for drivers

--- app/drivers/page.js
+++ app/drivers/page.js
@@ -1,25 +1,23 @@
+import { getData } from "../../lib/fetchData";
 import Card from "../../components/Card";
+import Link from "next/link";
 
-export default function DriversPage() {
-  // Placeholder driver data
-  const drivers = [
-    { id: 1, name: "Lewis Hamilton", team: "Mercedes" },
-    { id: 2, name: "Max Verstappen", team: "Red Bull Racing" },
-    { id: 3, name: "Charles Leclerc", team: "Ferrari" },
-    { id: 4, name: "Fernando Alonso", team: "Aston Martin" },
-  ];
+export default async function DriversPage() {
+  // Fetch Current Season Drivers
+  const data = await getData("/api/current/drivers");
 
   return (
     <section className="min-h-[80vh] container mx-auto p-4">
-      <h1 className="text-3xl font-bold text-red-600 mb-6">Drivers</h1>
+      <h1 className="text-3xl font-bold text-red-600 mb-6">Current Drivers</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {drivers.map((driver) => (
-          <Card
-            key={driver.id}
-            title={driver.name}
-            description={driver.team}
-          />
+        {data.drivers.map((driver) => (
+          <Link href={`/drivers/${driver.driverId}`} key={driver.driverId}>
+            <Card
+              title={`${driver.name} ${driver.surname}`}
+              description={`Nationality: ${driver.nationality}`}
+            />
+          </Link>
         ))}
       </div>
     </section>
