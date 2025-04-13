ee447aed690ab5ef558c4159d858195c02256873
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-04-13 17:19:00 -0600

    navbar auto hides when scrolling

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,38 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import SearchBar from "./SearchBar";
 
 export default function Navbar() {
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > lastScrollY) {
+        // Scrolling Down
+        setShowNavbar(false);
+      } else {
+        // Scrolling Up
+        setShowNavbar(true);
+      }
+      setLastScrollY(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
-    <nav className="fixed top-0 left-0 w-full z-50 bg-red-600/90 backdrop-blur-md p-4 flex flex-wrap justify-between items-center shadow-md shadow-red-900 gap-4">
-      
+    <nav
+      className={`fixed top-0 left-0 w-full z-50 bg-red-600/90 backdrop-blur-md p-4 flex flex-wrap justify-between items-center shadow-md shadow-red-900 gap-4 transition-transform duration-300 ${
+        showNavbar ? "translate-y-0" : "-translate-y-full"
+      }`}
+    >
       {/* Logo & Title */}
       <Link href="/" className="flex items-center gap-3 group">
         <div className="bg-black rounded-full w-13 h-13 flex items-center justify-center shadow-[0_0_10px_rgba(255,255,255,0.4)] group-hover:scale-105 transition-transform">
