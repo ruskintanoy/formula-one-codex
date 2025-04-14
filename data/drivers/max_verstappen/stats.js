a1a5af2c77403a48c01ba9daf71943487ae58cb4
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-04-13 23:02:07 -0600

    max stats done

--- data/drivers/max_verstappen/stats.js
+++ data/drivers/max_verstappen/stats.js
@@ -11,15 +11,15 @@ const wikiLink = (text, url) => (
 
 const stats = {
   "Seasons Active": "2015 - Present",
-      Championships: "4 (2021, 2022, 2023, 2024)",
-      Races: "213 (213 starts)",
-      Poles: 41,
-      Wins: 64,
-      Podiums: 114,
-      "Fastest Laps": 33,
-      Points: 3092.5,
-      "First Race": wikiLink("2015 Australian Grand Prix", "https://en.wikipedia.org/wiki/2015_Australian_Grand_Prix"),
-      "Last Win": wikiLink("2025 Japanese Grand Prix", "https://en.wikipedia.org/wiki/2025_Japanese_Grand_Prix"),
-    }
+  Championships: "4 (2021, 2022, 2023, 2024)",
+  Races: "213 (213 starts)",
+  Poles: 41,
+  Wins: 64,
+  Podiums: 114,
+  "Fastest Laps": 33,
+  Points: 3092.5,
+  "First Race": wikiLink("2015 Australian Grand Prix", "https://en.wikipedia.org/wiki/2015_Australian_Grand_Prix"),
+  "Last Win": wikiLink("2025 Japanese Grand Prix", "https://en.wikipedia.org/wiki/2025_Japanese_Grand_Prix"),
+  }
 
 export default stats;
