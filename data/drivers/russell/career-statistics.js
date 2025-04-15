e934663ff6fd8535b9293d9ae709e41cf1cfb1fa
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-04-15 03:06:56 -0600

    geroge career stats done

--- data/drivers/russell/career-statistics.js
+++ data/drivers/russell/career-statistics.js
@@ -1,27 +1,24 @@
 const careerStatistics = {
-    updatedTo: "2025 Australian Grand Prix",
-    stats: [
-      { label: "Entries", value: 125 },
-      { label: "Starts", value: 125 },
-      { label: "Pole Positions", value: 9 },
-      { label: "Front Row Starts", value: 15 },
-      { label: "Race Wins", value: 5 },
-      { label: "Podiums", value: 27 },
-      { label: "Fastest Laps", value: 13 },
-      { label: "Points", value: 1007 },
-      { label: "Laps Raced", value: 6880 },
-      { label: "Distance Raced", value: "34,691 km (21,556 mi)" },
-      { label: "Races Led", value: 17 },
-      { label: "Laps Led", value: 207 },
-      { label: "Distance Led", value: "1,054 km (655 mi)" },
-      { label: "Doubles", value: 2 },
-      { label: "Hat-Tricks", value: 1 },
-      { label: "Sprint Poles", value: 3 },
-      { label: "Sprint Wins", value: 1 },
-      { label: "Sprint Podiums", value: 6 },
-      { label: "Sprint Fastest Laps", value: 1 },
-    ],
-  };
-  
-  export default careerStatistics;
-  
+  updatedTo: "2025 Japanese Grand Prix",
+  stats: [
+    { label: "Entries", value: 125 },
+    { label: "Starts", value: 125 },
+    { label: "Pole Positions", value: 4, link: "https://en.wikipedia.org/wiki/Pole_position" },
+    { label: "Front Row Starts", value: 14 },
+    { label: "Race Wins", value: 3, link: "https://en.wikipedia.org/wiki/Victory" },
+    { label: "Podiums", value: 15, link: "https://en.wikipedia.org/wiki/Podium" },
+    { label: "Fastest Laps", value: 8, link: "https://en.wikipedia.org/wiki/Fastest_lap" },
+    { label: "Points", value: 714, link: "https://en.wikipedia.org/wiki/Points" },
+    { label: "Laps Raced", value: 6725 },
+    { label: "Distance Raced", value: "33,675 km (20,925 mi)" },
+    { label: "Races Led", value: 13 },
+    { label: "Laps Led", value: 225 },
+    { label: "Distance Led", value: "999 km (621 mi)" },
+    { label: "Doubles", value: 1 }, // Pole & Win same weekend
+    { label: "Sprint Wins", value: 1 },
+    { label: "Sprint Podiums", value: 2 },
+    { label: "Sprint Fastest Laps", value: 2 },
+  ],
+};
+
+export default careerStatistics;
