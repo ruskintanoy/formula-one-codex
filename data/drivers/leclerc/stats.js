a9517fd9d098d88adf646c832f02404c7c337498
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-04-15 15:37:55 -0600

    charles stats done

--- data/drivers/leclerc/stats.js
+++ data/drivers/leclerc/stats.js
@@ -10,16 +10,16 @@ const wikiLink = (text, url) => (
 );
 
 const stats = {
-  "Seasons Active": "2019 - Present",
+  "Seasons Active": "2018 - Present",
   Championships: 0,
-  Races: "132 (131 starts)",
-  Poles: 10,
-  Wins: 5,
-  Podiums: 29,
-  "Fastest Laps": 14,
-  Points: 1069,
-  "First Race": wikiLink("2019 Australian Grand Prix", "https://en.wikipedia.org/wiki/2019_Australian_Grand_Prix"),
-  "Last Win": wikiLink("2025 Australian Grand Prix", "https://en.wikipedia.org/wiki/2025_Australian_Grand_Prix"),
+  Races: "153 (151 starts)",
+  Poles: 26,
+  Wins: 8,
+  Podiums: 43,
+  "Fastest Laps": 10,
+  Points: 1462,
+  "First Race": wikiLink("2018 Australian Grand Prix", "https://en.wikipedia.org/wiki/2018_Australian_Grand_Prix"),
+  "Last Win": wikiLink("2024 United States Grand Prix", "https://en.wikipedia.org/wiki/2024_United_States_Grand_Prix"),
 };
 
 export default stats;
