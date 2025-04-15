0fcb4b4061df610498505f24ae3ec0dbe9ae1e9f
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-04-15 01:21:52 -0600

    george background done

--- data/drivers/russell/background.js
+++ data/drivers/russell/background.js
@@ -1,33 +1,54 @@
 const wikiLink = (text, url) => (
-    <a href={url} target="_blank" rel="noopener noreferrer" className="text-red-500 hover:underline"> {text}</a>);
-    
-  const background = (
-    <>
-      Lando Norris was born on 13 November 1999 in Bristol, England. His father Adam Norris is a retired pensions manager and is one of Bristol's wealthiest people as well as the 501st-richest person in the country (as of 2018).
-  
-      <br /><br />
-  
-      His mother Cisca (née Wauman) is from the Flanders region of Belgium. He has three siblings — two younger sisters, and an older brother Oliver, who was also involved in{" "}
-      {wikiLink("karting", "https://en.wikipedia.org/wiki/Karting")} on a competitive level until 2014.
-  
-      <br /><br />
-  
-      Norris holds both British and Belgian citizenship, and speaks a small amount of Flemish Dutch. In his early childhood, Norris tried horse riding, then quad biking and motorcycle riding before moving into karting after his father took him to watch the national British Karting Championships at age seven.
-  
-      <br /><br />
-  
-      Norris was educated at Millfield School in Street, Somerset. He left school without taking his GCSEs, but studied physics and mathematics with a full-time personal tutor. His family later moved to Glastonbury to allow him to become a day pupil and to pursue his racing career, citing{" "}
-      {wikiLink("Valentino Rossi", "https://en.wikipedia.org/wiki/Valentino_Rossi")} as an inspiration.
-  
-      <br /><br />
-  
-      At the start of his F1 career, he initially resided in Woking near the McLaren team headquarters, but later moved to Monaco in 2022 for financial reasons.
-  
-      <br /><br />
-  
-      Between August 2021 and September 2022, Norris dated Portuguese model Luísa Oliveira. Norris has stated that he and Oliveira were subjected to abuse and death threats from online trolls.
-    </>
-  );
-  
-  export default background;
-  
+  <a
+    href={url}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="text-red-500 hover:underline"
+  >
+    {" "}
+    {text}
+  </a>
+);
+
+const background = (
+  <>
+    George Russell was born on 15 February 1998 in{" "}
+    {wikiLink("King's Lynn", "https://en.wikipedia.org/wiki/King%27s_Lynn")},{" "}
+    {wikiLink("Norfolk", "https://en.wikipedia.org/wiki/Norfolk")}, to father Steve and mother Alison.
+
+    <br /><br />
+
+    His father managed a business selling seeds and{" "}
+    {wikiLink("pulses", "https://en.wikipedia.org/wiki/Pulses")} before stepping down to a consultant role in 2012. Russell is the youngest of three siblings, including sister Cara and brother Benjy.
+
+    <br /><br />
+
+    He grew up in{" "}
+    {wikiLink("Tydd St Giles", "https://en.wikipedia.org/wiki/Tydd_St_Giles")},{" "}
+    {wikiLink("Wisbech", "https://en.wikipedia.org/wiki/Wisbech")} and{" "}
+    {wikiLink("Castle Rising", "https://en.wikipedia.org/wiki/Castle_Rising")}.
+
+    <br /><br />
+
+    Russell began{" "}
+    {wikiLink("karting", "https://en.wikipedia.org/wiki/Karting")} at the age of seven, following his brother Benjy, who won the 2007{" "}
+    {wikiLink("Super 1 National Kart Championship", "https://en.wikipedia.org/wiki/Super_1_National_Kart_Championships")} in the Rotax Max category.
+
+    <br /><br />
+
+    Russell adopted his racing number 63 from the kart his brother rented at the time.
+
+    <br /><br />
+
+    He attended{" "}
+    {wikiLink("Wisbech Grammar School", "https://en.wikipedia.org/wiki/Wisbech_Grammar_School")} before shifting to homeschooling to focus on his racing career.
+
+    <br /><br />
+
+    At 18, Russell moved to{" "}
+    {wikiLink("Milton Keynes", "https://en.wikipedia.org/wiki/Milton_Keynes")} to be closer to his junior racing team, sharing accommodation with future Formula One driver{" "}
+    {wikiLink("Alex Albon", "https://en.wikipedia.org/wiki/Alex_Albon")}.
+  </>
+);
+
+export default background;
