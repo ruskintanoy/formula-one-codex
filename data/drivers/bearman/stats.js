377550e6dbda5fd07dcf4b9c40de285dd8c8af30
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-04-15 20:54:42 -0600

    olly stats done

--- data/drivers/bearman/stats.js
+++ data/drivers/bearman/stats.js
@@ -10,16 +10,15 @@ const wikiLink = (text, url) => (
 );
 
 const stats = {
-  "Seasons Active": "2019 - Present",
+  "Seasons Active": "2024 - Present",
   Championships: 0,
-  Races: "132 (131 starts)",
-  Poles: 10,
-  Wins: 5,
-  Podiums: 29,
-  "Fastest Laps": 14,
-  Points: 1069,
-  "First Race": wikiLink("2019 Australian Grand Prix", "https://en.wikipedia.org/wiki/2019_Australian_Grand_Prix"),
-  "Last Win": wikiLink("2025 Australian Grand Prix", "https://en.wikipedia.org/wiki/2025_Australian_Grand_Prix"),
+  Races: "7 (7 starts)",
+  Poles: 0,
+  Wins: 0,
+  Podiums: 0,
+  "Fastest Laps": 0,
+  Points: 13,
+  "First Race": wikiLink("2024 Saudi Arabian Grand Prix", "https://en.wikipedia.org/wiki/2024_Saudi_Arabian_Grand_Prix"),
 };
 
 export default stats;
