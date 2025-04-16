fe61e1522b56c2a8302631eb9cb3f31dc3733043
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-04-15 18:57:38 -0600

    kimi background done

--- data/drivers/antonelli/background.js
+++ data/drivers/antonelli/background.js
@@ -1,33 +1,53 @@
 const wikiLink = (text, url) => (
-    <a href={url} target="_blank" rel="noopener noreferrer" className="text-red-500 hover:underline"> {text}</a>);
-    
-  const background = (
-    <>
-      Lando Norris was born on 13 November 1999 in Bristol, England. His father Adam Norris is a retired pensions manager and is one of Bristol's wealthiest people as well as the 501st-richest person in the country (as of 2018).
-  
-      <br /><br />
-  
-      His mother Cisca (née Wauman) is from the Flanders region of Belgium. He has three siblings — two younger sisters, and an older brother Oliver, who was also involved in{" "}
-      {wikiLink("karting", "https://en.wikipedia.org/wiki/Karting")} on a competitive level until 2014.
-  
-      <br /><br />
-  
-      Norris holds both British and Belgian citizenship, and speaks a small amount of Flemish Dutch. In his early childhood, Norris tried horse riding, then quad biking and motorcycle riding before moving into karting after his father took him to watch the national British Karting Championships at age seven.
-  
-      <br /><br />
-  
-      Norris was educated at Millfield School in Street, Somerset. He left school without taking his GCSEs, but studied physics and mathematics with a full-time personal tutor. His family later moved to Glastonbury to allow him to become a day pupil and to pursue his racing career, citing{" "}
-      {wikiLink("Valentino Rossi", "https://en.wikipedia.org/wiki/Valentino_Rossi")} as an inspiration.
-  
-      <br /><br />
-  
-      At the start of his F1 career, he initially resided in Woking near the McLaren team headquarters, but later moved to Monaco in 2022 for financial reasons.
-  
-      <br /><br />
-  
-      Between August 2021 and September 2022, Norris dated Portuguese model Luísa Oliveira. Norris has stated that he and Oliveira were subjected to abuse and death threats from online trolls.
-    </>
-  );
-  
-  export default background;
-  
+  <a href={url} target="_blank" rel="noopener noreferrer" className="text-red-500 hover:underline">
+    {text}
+  </a>
+);
+
+const background = (
+  <>
+    Andrea Kimi Antonelli was born on 25 August 2006 in{" "}
+    {wikiLink("Bologna", "https://en.wikipedia.org/wiki/Bologna")}, Emilia-Romagna, Italy.
+
+    <br /><br />
+
+    His father{" "}
+    {wikiLink("Marco Antonelli", "https://en.wikipedia.org/wiki/Marco_Antonelli")} is a{" "}
+    {wikiLink("sportscar racing", "https://en.wikipedia.org/wiki/Sportscar_racing")} driver and owner of{" "}
+    <i>AKM Motorsport</i>, a team based in{" "}
+    {wikiLink("San Marino", "https://en.wikipedia.org/wiki/San_Marino")} that has competed in the{" "}
+    {wikiLink("Italian F4 Championship", "https://en.wikipedia.org/wiki/Italian_F4_Championship")} since{" "}
+    {wikiLink("2022", "https://en.wikipedia.org/wiki/2022_Italian_F4_Championship")}.
+
+    <br /><br />
+
+    He formerly operated <i>Antonelli Motorsport</i>, which competed in the{" "}
+    {wikiLink("Blancpain GT Series", "https://en.wikipedia.org/wiki/Blancpain_GT_Series")},{" "}
+    {wikiLink("Porsche Supercup", "https://en.wikipedia.org/wiki/Porsche_Supercup")} and{" "}
+    {wikiLink("International GT Open", "https://en.wikipedia.org/wiki/International_GT_Open")}, winning the{" "}
+    {wikiLink("2018 Italian GT Championship", "https://en.wikipedia.org/wiki/2018_Italian_GT_Championship")}.
+
+    <br /><br />
+
+    Antonelli shares his middle name with the forename of{" "}
+    {wikiLink("Kimi Räikkönen", "https://en.wikipedia.org/wiki/Kimi_R%C3%A4ikk%C3%B6nen")}, the{" "}
+    {wikiLink("2007 Formula One World Drivers' Champion", "https://en.wikipedia.org/wiki/2007_Formula_One_World_Championship")}. He has stated that the name was given by{" "}
+    {wikiLink("Enrico Bertaggia", "https://en.wikipedia.org/wiki/Enrico_Bertaggia")} to his father, who wanted a foreign-sounding name after "Andrea", and that he was not named after Räikkönen.
+
+    <br /><br />
+
+    Antonelli attended <i>ITCS Gaetano Salvemini</i> in{" "}
+    {wikiLink("Casalecchio di Reno", "https://en.wikipedia.org/wiki/Casalecchio_di_Reno")}, where he studied{" "}
+    {wikiLink("international relations", "https://en.wikipedia.org/wiki/International_relations")} and{" "}
+    {wikiLink("marketing", "https://en.wikipedia.org/wiki/Marketing")}. He learned to speak English during race weekends.
+
+    <br /><br />
+
+    His father initially encouraged him to pursue{" "}
+    {wikiLink("association football", "https://en.wikipedia.org/wiki/Association_football")}, but Antonelli showed greater passion for{" "}
+    {wikiLink("motorsport", "https://en.wikipedia.org/wiki/Motorsport")}, leading to his early entry into{" "}
+    {wikiLink("karting", "https://en.wikipedia.org/wiki/Karting")}.
+  </>
+);
+
+export default background;
