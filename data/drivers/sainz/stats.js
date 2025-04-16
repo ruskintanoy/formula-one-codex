6641226586081ec05734f28239452026ecb8c7e1
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-04-15 20:59:11 -0600

    carlos stats done

--- data/drivers/sainz/stats.js
+++ data/drivers/sainz/stats.js
@@ -10,16 +10,16 @@ const wikiLink = (text, url) => (
 );
 
 const stats = {
-  "Seasons Active": "2019 - Present",
+  "Seasons Active": "2015 - Present",
   Championships: 0,
-  Races: "132 (131 starts)",
-  Poles: 10,
-  Wins: 5,
-  Podiums: 29,
-  "Fastest Laps": 14,
-  Points: 1069,
-  "First Race": wikiLink("2019 Australian Grand Prix", "https://en.wikipedia.org/wiki/2019_Australian_Grand_Prix"),
-  "Last Win": wikiLink("2025 Australian Grand Prix", "https://en.wikipedia.org/wiki/2025_Australian_Grand_Prix"),
+  Races: "213 (210 starts)",
+  Poles: 6,
+  Wins: 4,
+  Podiums: 27,
+  "Fastest Laps": 4,
+  Points: 1273.5,
+  "First Race": wikiLink("2015 Australian Grand Prix", "https://en.wikipedia.org/wiki/2015_Australian_Grand_Prix"),
+  "Last Win": wikiLink("2024 Mexico city Grand Prix", "https://en.wikipedia.org/wiki/2024_Mexico_City_Grand_Prix"),
 };
 
 export default stats;
