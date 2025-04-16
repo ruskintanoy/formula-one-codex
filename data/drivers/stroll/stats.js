dd24ebeda007a5dda32bc48fac96ae5462f53b6c
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-04-15 20:48:02 -0600

    stroll stats done

--- data/drivers/stroll/stats.js
+++ data/drivers/stroll/stats.js
@@ -10,16 +10,15 @@ const wikiLink = (text, url) => (
 );
 
 const stats = {
-  "Seasons Active": "2019 - Present",
+  "Seasons Active": "2017 - Present",
   Championships: 0,
-  Races: "132 (131 starts)",
-  Poles: 10,
-  Wins: 5,
-  Podiums: 29,
-  "Fastest Laps": 14,
-  Points: 1069,
-  "First Race": wikiLink("2019 Australian Grand Prix", "https://en.wikipedia.org/wiki/2019_Australian_Grand_Prix"),
-  "Last Win": wikiLink("2025 Australian Grand Prix", "https://en.wikipedia.org/wiki/2025_Australian_Grand_Prix"),
+  Races: "173 (170 starts)",
+  Poles: 0,
+  Wins: 0,
+  Podiums: 3,
+  "Fastest Laps": 0,
+  Points: 302,
+  "First Race": wikiLink("2017 Australian Grand Prix", "https://en.wikipedia.org/wiki/2017_Australian_Grand_Prix"),
 };
 
 export default stats;
