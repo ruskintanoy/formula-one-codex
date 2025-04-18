bad0cdbb793c296852357f6f65fae8b6fc81db02
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-04-18 02:41:00 -0600

    bearman bg done

--- data/drivers/bearman/background.js
+++ data/drivers/bearman/background.js
@@ -1,33 +1,24 @@
 const wikiLink = (text, url) => (
-    <a href={url} target="_blank" rel="noopener noreferrer" className="text-red-500 hover:underline"> {text}</a>);
-    
-  const background = (
-    <>
-      Lando Norris was born on 13 November 1999 in Bristol, England. His father Adam Norris is a retired pensions manager and is one of Bristol's wealthiest people as well as the 501st-richest person in the country (as of 2018).
-  
-      <br /><br />
-  
-      His mother Cisca (née Wauman) is from the Flanders region of Belgium. He has three siblings — two younger sisters, and an older brother Oliver, who was also involved in{" "}
-      {wikiLink("karting", "https://en.wikipedia.org/wiki/Karting")} on a competitive level until 2014.
-  
-      <br /><br />
-  
-      Norris holds both British and Belgian citizenship, and speaks a small amount of Flemish Dutch. In his early childhood, Norris tried horse riding, then quad biking and motorcycle riding before moving into karting after his father took him to watch the national British Karting Championships at age seven.
-  
-      <br /><br />
-  
-      Norris was educated at Millfield School in Street, Somerset. He left school without taking his GCSEs, but studied physics and mathematics with a full-time personal tutor. His family later moved to Glastonbury to allow him to become a day pupil and to pursue his racing career, citing{" "}
-      {wikiLink("Valentino Rossi", "https://en.wikipedia.org/wiki/Valentino_Rossi")} as an inspiration.
-  
-      <br /><br />
-  
-      At the start of his F1 career, he initially resided in Woking near the McLaren team headquarters, but later moved to Monaco in 2022 for financial reasons.
-  
-      <br /><br />
-  
-      Between August 2021 and September 2022, Norris dated Portuguese model Luísa Oliveira. Norris has stated that he and Oliveira were subjected to abuse and death threats from online trolls.
-    </>
-  );
-  
-  export default background;
-  
+  <a href={url} target="_blank" rel="noopener noreferrer" className="text-red-500 hover:underline">
+    {text}
+  </a>
+);
+
+const background = (
+  <>
+    Bearman was born on 8 May 2005 to David and Terri Bearman in{" "}
+    {wikiLink("Havering", "https://en.wikipedia.org/wiki/London_Borough_of_Havering")}, London. He has a younger brother, Thomas Bearman (who is also a racing driver), and a sister. His father is the founder and chief executive officer of the insurance firm Aventum Group. Bearman grew up in{" "}
+    {wikiLink("Chelmsford", "https://en.wikipedia.org/wiki/Chelmsford")}, Essex, where he attended{" "}
+    {wikiLink("King Edward VI Grammar School", "https://en.wikipedia.org/wiki/King_Edward_VI_Grammar_School,_Chelmsford")}. Bearman left school at the age of 16 to join the{" "}
+    {wikiLink("Ferrari Driver Academy", "https://en.wikipedia.org/wiki/Ferrari_Driver_Academy")} in{" "}
+    {wikiLink("Modena", "https://en.wikipedia.org/wiki/Modena")}, Italy.
+
+    <br /><br />
+
+    Bearman chose 87 as his permanent racing number for{" "}
+    {wikiLink("Formula One", "https://en.wikipedia.org/wiki/Formula_One")}, as it was the number that he first used in{" "}
+    {wikiLink("kart racing", "https://en.wikipedia.org/wiki/Kart_racing")}.
+  </>
+);
+
+export default background;
