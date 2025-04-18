55af3fa7cb676b39b7e20bbc42158b2fbaf689d6
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-04-18 01:35:11 -0600

    gasly background done

--- data/drivers/gasly/background.js
+++ data/drivers/gasly/background.js
@@ -1,33 +1,40 @@
 const wikiLink = (text, url) => (
-    <a href={url} target="_blank" rel="noopener noreferrer" className="text-red-500 hover:underline"> {text}</a>);
-    
-  const background = (
-    <>
-      Lando Norris was born on 13 November 1999 in Bristol, England. His father Adam Norris is a retired pensions manager and is one of Bristol's wealthiest people as well as the 501st-richest person in the country (as of 2018).
-  
-      <br /><br />
-  
-      His mother Cisca (née Wauman) is from the Flanders region of Belgium. He has three siblings — two younger sisters, and an older brother Oliver, who was also involved in{" "}
-      {wikiLink("karting", "https://en.wikipedia.org/wiki/Karting")} on a competitive level until 2014.
-  
-      <br /><br />
-  
-      Norris holds both British and Belgian citizenship, and speaks a small amount of Flemish Dutch. In his early childhood, Norris tried horse riding, then quad biking and motorcycle riding before moving into karting after his father took him to watch the national British Karting Championships at age seven.
-  
-      <br /><br />
-  
-      Norris was educated at Millfield School in Street, Somerset. He left school without taking his GCSEs, but studied physics and mathematics with a full-time personal tutor. His family later moved to Glastonbury to allow him to become a day pupil and to pursue his racing career, citing{" "}
-      {wikiLink("Valentino Rossi", "https://en.wikipedia.org/wiki/Valentino_Rossi")} as an inspiration.
-  
-      <br /><br />
-  
-      At the start of his F1 career, he initially resided in Woking near the McLaren team headquarters, but later moved to Monaco in 2022 for financial reasons.
-  
-      <br /><br />
-  
-      Between August 2021 and September 2022, Norris dated Portuguese model Luísa Oliveira. Norris has stated that he and Oliveira were subjected to abuse and death threats from online trolls.
-    </>
-  );
-  
-  export default background;
-  
+  <a href={url} target="_blank" rel="noopener noreferrer" className="text-red-500 hover:underline">
+    {text}
+  </a>
+);
+
+const background = (
+  <>
+    Pierre Jean-Jacques Gasly was born on 7 February 1996 in{" "}
+    {wikiLink("Rouen", "https://en.wikipedia.org/wiki/Rouen")}, France, to father Jean-Jacques Gasly and mother Pascale. Gasly is the youngest of five and has four{" "}
+    {wikiLink("half brothers", "https://en.wikipedia.org/wiki/Half_brother")}: two{" "}
+    {wikiLink("maternal", "https://en.wikipedia.org/wiki/Maternal")} from his mother's previous marriage, Nicolas Caron and Cyril Caron; and two{" "}
+    {wikiLink("paternal", "https://en.wikipedia.org/wiki/Paternal")} from his father's previous marriage, Phillipe Gasly and Paul Gasly. Gasly was raised Christian and routinely makes the sign of the cross to bless himself before races.
+
+    <br /><br />
+
+    Gasly's family has long been involved in{" "}
+    {wikiLink("motorsports", "https://en.wikipedia.org/wiki/Motorsport")}. His grandfather competed in{" "}
+    {wikiLink("karting", "https://en.wikipedia.org/wiki/Kart_racing")}, his grandmother a kart champion, and his father Jean Jacques has also competed in various categories of racing including{" "}
+    {wikiLink("karting", "https://en.wikipedia.org/wiki/Kart_racing")},{" "}
+    {wikiLink("endurance racing", "https://en.wikipedia.org/wiki/Endurance_racing_(motorsport)")} and{" "}
+    {wikiLink("rallying", "https://en.wikipedia.org/wiki/Rallying")}. His father stopped rallying when he 'fell off [a] mountain' after his co-driver made a mistake reading the pace note. At the age of six, Gasly first experienced karting at a local karting track in{" "}
+    {wikiLink("Anneville-Ambourville", "https://en.wikipedia.org/wiki/Anneville-Ambourville")}. He was 13 years old when he left Rouen for Le Mans after his commitment for competitive racing.
+
+    <br /><br />
+
+    Gasly grew up alongside{" "}
+    {wikiLink("Anthoine Hubert", "https://en.wikipedia.org/wiki/Anthoine_Hubert")}; karting with him since the age of seven, being educated at the same private school and having resided together as{" "}
+    {wikiLink("roommates", "https://en.wikipedia.org/wiki/Roommate")} for several years. He has been close friends with{" "}
+    {wikiLink("Charles Leclerc", "https://en.wikipedia.org/wiki/Charles_Leclerc")} and{" "}
+    {wikiLink("Esteban Ocon", "https://en.wikipedia.org/wiki/Esteban_Ocon")} since a young age. However, his relationship with Ocon deteriorated during their karting career. In 2019 he moved to{" "}
+    {wikiLink("Milan", "https://en.wikipedia.org/wiki/Milan")}. Besides his native French, Gasly also speaks Italian and English.
+
+    <br /><br />
+
+    Gasly has been dating Francisca Gomes since October 2022. Gomes is a model and influencer and she is signed to the Portugal-based modeling agency Central Models. The couple went official in the start of 2023.
+  </>
+);
+
+export default background;
