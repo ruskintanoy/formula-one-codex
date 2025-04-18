cf66a7f82f8501b2d17401d45ef1c988bb6a2c6e
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-04-18 02:43:44 -0600

    sainz bg done

--- data/drivers/sainz/background.js
+++ data/drivers/sainz/background.js
@@ -1,33 +1,25 @@
 const wikiLink = (text, url) => (
-    <a href={url} target="_blank" rel="noopener noreferrer" className="text-red-500 hover:underline"> {text}</a>);
-    
-  const background = (
-    <>
-      Lando Norris was born on 13 November 1999 in Bristol, England. His father Adam Norris is a retired pensions manager and is one of Bristol's wealthiest people as well as the 501st-richest person in the country (as of 2018).
-  
-      <br /><br />
-  
-      His mother Cisca (née Wauman) is from the Flanders region of Belgium. He has three siblings — two younger sisters, and an older brother Oliver, who was also involved in{" "}
-      {wikiLink("karting", "https://en.wikipedia.org/wiki/Karting")} on a competitive level until 2014.
-  
-      <br /><br />
-  
-      Norris holds both British and Belgian citizenship, and speaks a small amount of Flemish Dutch. In his early childhood, Norris tried horse riding, then quad biking and motorcycle riding before moving into karting after his father took him to watch the national British Karting Championships at age seven.
-  
-      <br /><br />
-  
-      Norris was educated at Millfield School in Street, Somerset. He left school without taking his GCSEs, but studied physics and mathematics with a full-time personal tutor. His family later moved to Glastonbury to allow him to become a day pupil and to pursue his racing career, citing{" "}
-      {wikiLink("Valentino Rossi", "https://en.wikipedia.org/wiki/Valentino_Rossi")} as an inspiration.
-  
-      <br /><br />
-  
-      At the start of his F1 career, he initially resided in Woking near the McLaren team headquarters, but later moved to Monaco in 2022 for financial reasons.
-  
-      <br /><br />
-  
-      Between August 2021 and September 2022, Norris dated Portuguese model Luísa Oliveira. Norris has stated that he and Oliveira were subjected to abuse and death threats from online trolls.
-    </>
-  );
-  
-  export default background;
-  
+  <a href={url} target="_blank" rel="noopener noreferrer" className="text-red-500 hover:underline">
+    {text}
+  </a>
+);
+
+const background = (
+  <>
+    Carlos Sainz Vázquez de Castro was born on 1 September 1994 in{" "}
+    {wikiLink("Madrid", "https://en.wikipedia.org/wiki/Madrid")}, Spain, to father{" "}
+    {wikiLink("Carlos Sainz", "https://en.wikipedia.org/wiki/Carlos_Sainz_Sr.")} and his wife Reyes Vázquez de Castro. His father is a{" "}
+    {wikiLink("rally driver", "https://en.wikipedia.org/wiki/Rally_driver")}, who won the{" "}
+    {wikiLink("World Rally Championship", "https://en.wikipedia.org/wiki/World_Rally_Championship")} twice in{" "}
+    {wikiLink("1990", "https://en.wikipedia.org/wiki/1990_World_Rally_Championship")} and{" "}
+    {wikiLink("1992", "https://en.wikipedia.org/wiki/1992_World_Rally_Championship")}, with 26{" "}
+    {wikiLink("rally victories", "https://en.wikipedia.org/wiki/List_of_World_Rally_Championship_event_winners")}. Sainz Jr. was mentored by his father throughout his journey to F1.
+
+    <br /><br />
+
+    In 2016, Sainz was named Ambassador of{" "}
+    {wikiLink("María de Villota", "https://en.wikipedia.org/wiki/Mar%C3%ADa_de_Villota")}'s Legacy, as he was coached by her at driving school in Madrid. He has spoken about how he was personally affected by her death in 2013, and he has kept a star on his helmet since 2014 in honor of her.
+  </>
+);
+
+export default background;
