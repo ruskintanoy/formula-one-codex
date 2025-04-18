a96a201aac613c7c743e62695336e575b2935689
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-04-17 21:22:27 -0600

    update background

--- data/drivers/piastri/background.js
+++ data/drivers/piastri/background.js
@@ -8,12 +8,12 @@ const background = (
       He claims Italian, Yugoslavian and Chinese heritage from his father's side as well as Scottish and Irish heritage from his mother's side.
       He was privately educated at Haileybury College Melbourne and then Haileybury and Imperial Service College in England as a boarding pupil on a sports scholarship.
 
-    <br></br>
+    <br /><br />
 
       Chris Piastri served as his son's mechanic during Oscar's early racing years in Australia and is the founder and owner of HP Tuners, a vehicle diagnostics automotive software company.
       He is a keen Richmond Tigers supporter. As well as supporting Australia's national cricket team, he follows the Delhi Capitals in the IPL, after a post on his X profile asking for recommendations of which team to support.
 
-    <br></br>
+    <br /><br />
 
       As of July 2024, Piastri is dating Lily Zneimer, an engineering graduate whom he met during his schoolgoing years. 
       The pair began dating the year before Piastri completed his education in 2019.
