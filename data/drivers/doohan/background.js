1a8c018247cd0556451554ddacfedc81974ab4ff
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-04-18 02:46:00 -0600

    doohan bg done

--- data/drivers/doohan/background.js
+++ data/drivers/doohan/background.js
@@ -1,33 +1,17 @@
 const wikiLink = (text, url) => (
-    <a href={url} target="_blank" rel="noopener noreferrer" className="text-red-500 hover:underline"> {text}</a>);
-    
-  const background = (
-    <>
-      Lando Norris was born on 13 November 1999 in Bristol, England. His father Adam Norris is a retired pensions manager and is one of Bristol's wealthiest people as well as the 501st-richest person in the country (as of 2018).
-  
-      <br /><br />
-  
-      His mother Cisca (née Wauman) is from the Flanders region of Belgium. He has three siblings — two younger sisters, and an older brother Oliver, who was also involved in{" "}
-      {wikiLink("karting", "https://en.wikipedia.org/wiki/Karting")} on a competitive level until 2014.
-  
-      <br /><br />
-  
-      Norris holds both British and Belgian citizenship, and speaks a small amount of Flemish Dutch. In his early childhood, Norris tried horse riding, then quad biking and motorcycle riding before moving into karting after his father took him to watch the national British Karting Championships at age seven.
-  
-      <br /><br />
-  
-      Norris was educated at Millfield School in Street, Somerset. He left school without taking his GCSEs, but studied physics and mathematics with a full-time personal tutor. His family later moved to Glastonbury to allow him to become a day pupil and to pursue his racing career, citing{" "}
-      {wikiLink("Valentino Rossi", "https://en.wikipedia.org/wiki/Valentino_Rossi")} as an inspiration.
-  
-      <br /><br />
-  
-      At the start of his F1 career, he initially resided in Woking near the McLaren team headquarters, but later moved to Monaco in 2022 for financial reasons.
-  
-      <br /><br />
-  
-      Between August 2021 and September 2022, Norris dated Portuguese model Luísa Oliveira. Norris has stated that he and Oliveira were subjected to abuse and death threats from online trolls.
-    </>
-  );
-  
-  export default background;
-  
+  <a href={url} target="_blank" rel="noopener noreferrer" className="text-red-500 hover:underline">
+    {text}
+  </a>
+);
+
+const background = (
+  <>
+    Jack Doohan was born on 20 January 2003 in{" "}
+    {wikiLink("Gold Coast, Queensland", "https://en.wikipedia.org/wiki/Gold_Coast,_Queensland")}. He attended{" "}
+    {wikiLink("The Southport School", "https://en.wikipedia.org/wiki/The_Southport_School")} throughout his upbringing. Doohan is the son of five-time{" "}
+    {wikiLink("Grand Prix motorcycle World Champion", "https://en.wikipedia.org/wiki/List_of_500cc/MotoGP_Motorcycle_World_Champions")}{" "}
+    {wikiLink("Mick Doohan", "https://en.wikipedia.org/wiki/Mick_Doohan")}.
+  </>
+);
+
+export default background;
