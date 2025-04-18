0c7ed8f50d0d3665043c87b7c1d8b55df285953e
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-04-18 02:42:45 -0600

    isack bg done

--- data/drivers/hadjar/background.js
+++ data/drivers/hadjar/background.js
@@ -1,33 +1,29 @@
 const wikiLink = (text, url) => (
-    <a href={url} target="_blank" rel="noopener noreferrer" className="text-red-500 hover:underline"> {text}</a>);
-    
-  const background = (
-    <>
-      Lando Norris was born on 13 November 1999 in Bristol, England. His father Adam Norris is a retired pensions manager and is one of Bristol's wealthiest people as well as the 501st-richest person in the country (as of 2018).
-  
-      <br /><br />
-  
-      His mother Cisca (née Wauman) is from the Flanders region of Belgium. He has three siblings — two younger sisters, and an older brother Oliver, who was also involved in{" "}
-      {wikiLink("karting", "https://en.wikipedia.org/wiki/Karting")} on a competitive level until 2014.
-  
-      <br /><br />
-  
-      Norris holds both British and Belgian citizenship, and speaks a small amount of Flemish Dutch. In his early childhood, Norris tried horse riding, then quad biking and motorcycle riding before moving into karting after his father took him to watch the national British Karting Championships at age seven.
-  
-      <br /><br />
-  
-      Norris was educated at Millfield School in Street, Somerset. He left school without taking his GCSEs, but studied physics and mathematics with a full-time personal tutor. His family later moved to Glastonbury to allow him to become a day pupil and to pursue his racing career, citing{" "}
-      {wikiLink("Valentino Rossi", "https://en.wikipedia.org/wiki/Valentino_Rossi")} as an inspiration.
-  
-      <br /><br />
-  
-      At the start of his F1 career, he initially resided in Woking near the McLaren team headquarters, but later moved to Monaco in 2022 for financial reasons.
-  
-      <br /><br />
-  
-      Between August 2021 and September 2022, Norris dated Portuguese model Luísa Oliveira. Norris has stated that he and Oliveira were subjected to abuse and death threats from online trolls.
-    </>
-  );
-  
-  export default background;
-  
+  <a href={url} target="_blank" rel="noopener noreferrer" className="text-red-500 hover:underline">
+    {text}
+  </a>
+);
+
+const background = (
+  <>
+    Isack Alexandre Hadjar was born on 28 September 2004 in{" "}
+    {wikiLink("Paris", "https://en.wikipedia.org/wiki/Paris")}, France to an Algerian family of{" "}
+    {wikiLink("physicians", "https://en.wikipedia.org/wiki/Physician")} and{" "}
+    {wikiLink("physicists", "https://en.wikipedia.org/wiki/Physicist")}. His father, Yassine Hadjar, is a{" "}
+    {wikiLink("researcher", "https://en.wikipedia.org/wiki/Scientific_method")} in{" "}
+    {wikiLink("quantum mechanics", "https://en.wikipedia.org/wiki/Quantum_mechanics")} and also served as his kart{" "}
+    {wikiLink("mechanic", "https://en.wikipedia.org/wiki/Mechanic")}. He holds dual French and Algerian citizenship.
+
+    <br /><br />
+
+    Hadjar initially grew fond of{" "}
+    {wikiLink("motorsport", "https://en.wikipedia.org/wiki/Motorsport")} after watching the{" "}
+    {wikiLink("Pixar", "https://en.wikipedia.org/wiki/Pixar")} animated movie <i>{wikiLink("Cars", "https://en.wikipedia.org/wiki/Cars_(film)")}</i>. His parents bought him a{" "}
+    {wikiLink("go-kart", "https://en.wikipedia.org/wiki/Go-kart")} when he was seven, a year after he started watching{" "}
+    {wikiLink("Formula One", "https://en.wikipedia.org/wiki/Formula_One")}. He also contested{" "}
+    {wikiLink("boxing", "https://en.wikipedia.org/wiki/Boxing")} and{" "}
+    {wikiLink("judo", "https://en.wikipedia.org/wiki/Judo")} at a young age.
+  </>
+);
+
+export default background;
