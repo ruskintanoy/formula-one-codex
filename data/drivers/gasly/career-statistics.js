8c3f22911654f6a6551a18aca78e678e6a050578
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-04-18 02:22:38 -0600

    gasly career stats done

--- data/drivers/gasly/career-statistics.js
+++ data/drivers/gasly/career-statistics.js
@@ -1,27 +1,32 @@
+const wikiLink = (text, url) => (
+  <a
+    href={url}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="text-red-500 hover:underline"
+  >
+    {text}
+  </a>
+);
+
 const careerStatistics = {
-    updatedTo: "2025 Australian Grand Prix",
-    stats: [
-      { label: "Entries", value: 125 },
-      { label: "Starts", value: 125 },
-      { label: "Pole Positions", value: 9 },
-      { label: "Front Row Starts", value: 15 },
-      { label: "Race Wins", value: 5 },
-      { label: "Podiums", value: 27 },
-      { label: "Fastest Laps", value: 13 },
-      { label: "Points", value: 1007 },
-      { label: "Laps Raced", value: 6880 },
-      { label: "Distance Raced", value: "34,691 km (21,556 mi)" },
-      { label: "Races Led", value: 17 },
-      { label: "Laps Led", value: 207 },
-      { label: "Distance Led", value: "1,054 km (655 mi)" },
-      { label: "Doubles", value: 2 },
-      { label: "Hat-Tricks", value: 1 },
-      { label: "Sprint Poles", value: 3 },
-      { label: "Sprint Wins", value: 1 },
-      { label: "Sprint Podiums", value: 6 },
-      { label: "Sprint Fastest Laps", value: 1 },
-    ],
-  };
-  
-  export default careerStatistics;
-  
+  updatedTo: wikiLink("2025 Australian Grand Prix", "https://f1.fandom.com/wiki/2025_Australian_Grand_Prix"),
+  stats: [
+    { label: "Entries", value: 151 },
+    { label: "Starts", value: 150 },
+    { label: "Pole Positions", value: 0 },
+    { label: "Front Row Starts", value: 1 },
+    { label: "Race Wins", value: 1 },
+    { label: "Podiums", value: 5 },
+    { label: "Fastest Laps", value: 3 },
+    { label: "Points", value: 436 },
+    { label: "Laps Raced", value: 7918 },
+    { label: "Distance Raced", value: "39,726 km (24,685 mi)" },
+    { label: "Races Led", value: 1 },
+    { label: "Laps Led", value: 26 },
+    { label: "Distance Led", value: "151 km (94 mi)" },
+    { label: "Sprint Podiums", value: 1 },
+  ],
+};
+
+export default careerStatistics;
