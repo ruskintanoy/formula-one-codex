2b62c522f1422943f1a8a7a5a4a57fc4934cb50b
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-04-17 23:21:41 -0600

    stroll bg done

--- data/drivers/stroll/background.js
+++ data/drivers/stroll/background.js
@@ -1,33 +1,23 @@
 const wikiLink = (text, url) => (
-    <a href={url} target="_blank" rel="noopener noreferrer" className="text-red-500 hover:underline"> {text}</a>);
-    
-  const background = (
-    <>
-      Lando Norris was born on 13 November 1999 in Bristol, England. His father Adam Norris is a retired pensions manager and is one of Bristol's wealthiest people as well as the 501st-richest person in the country (as of 2018).
-  
-      <br /><br />
-  
-      His mother Cisca (née Wauman) is from the Flanders region of Belgium. He has three siblings — two younger sisters, and an older brother Oliver, who was also involved in{" "}
-      {wikiLink("karting", "https://en.wikipedia.org/wiki/Karting")} on a competitive level until 2014.
-  
-      <br /><br />
-  
-      Norris holds both British and Belgian citizenship, and speaks a small amount of Flemish Dutch. In his early childhood, Norris tried horse riding, then quad biking and motorcycle riding before moving into karting after his father took him to watch the national British Karting Championships at age seven.
-  
-      <br /><br />
-  
-      Norris was educated at Millfield School in Street, Somerset. He left school without taking his GCSEs, but studied physics and mathematics with a full-time personal tutor. His family later moved to Glastonbury to allow him to become a day pupil and to pursue his racing career, citing{" "}
-      {wikiLink("Valentino Rossi", "https://en.wikipedia.org/wiki/Valentino_Rossi")} as an inspiration.
-  
-      <br /><br />
-  
-      At the start of his F1 career, he initially resided in Woking near the McLaren team headquarters, but later moved to Monaco in 2022 for financial reasons.
-  
-      <br /><br />
-  
-      Between August 2021 and September 2022, Norris dated Portuguese model Luísa Oliveira. Norris has stated that he and Oliveira were subjected to abuse and death threats from online trolls.
-    </>
-  );
-  
-  export default background;
-  
+  <a href={url} target="_blank" rel="noopener noreferrer" className="text-red-500 hover:underline">
+    {text}
+  </a>
+);
+
+const background = (
+  <>
+    Lance Strulovitch was born on 29 October 1998 in{" "}
+    {wikiLink("Montreal", "https://en.wikipedia.org/wiki/Montreal")}. Stroll is the son of Canadian billionaire businessman{" "}
+    {wikiLink("Lawrence Stroll", "https://en.wikipedia.org/wiki/Lawrence_Stroll")} (part-owner of the{" "}
+    {wikiLink("Aston Martin Formula One team", "https://en.wikipedia.org/wiki/Aston_Martin_in_Formula_One")}) and Belgian fashion designer Claire-Anne Callens, and has an older sister named{" "}
+    {wikiLink("Chloe", "https://en.wikipedia.org/wiki/Chloe_Stroll")}. His parents are divorced and his father has since remarried to Raquel Diniz. He is of{" "}
+    {wikiLink("Russian Jewish", "https://en.wikipedia.org/wiki/Russian_Jewish")} descent from his father's side. Stroll races under the Canadian flag and holds both Canadian and Belgian citizenship. 
+    He is multilingual, speaking{" "}
+    {wikiLink("English", "https://en.wikipedia.org/wiki/English_language")},{" "}
+    {wikiLink("French", "https://en.wikipedia.org/wiki/French_language")},{" "}
+    {wikiLink("Flemish Dutch", "https://en.wikipedia.org/wiki/Flemish_dialects")}, and{" "}
+    {wikiLink("Italian", "https://en.wikipedia.org/wiki/Italian_language")}.
+  </>
+);
+
+export default background;
