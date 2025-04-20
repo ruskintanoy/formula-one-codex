567af13e674a4fd71191ff74c288d8003fe1f81c
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-04-20 01:43:46 -0600

    aston martin stats done

--- data/teams/aston_martin/info.js
+++ data/teams/aston_martin/info.js
@@ -1,51 +1,49 @@
 const wikiLink = (text, url) => (
-    <a href={url} target="_blank" rel="noopener noreferrer" className="text-red-500 hover:underline">
-      {text}
-    </a>
-  );
-  
-  export default {
-    base: <>
-      {wikiLink("Enstone, United Kingdom", "https://en.wikipedia.org/wiki/Enstone")}
-    </>,
+  <a href={url} target="_blank" rel="noopener noreferrer" className="text-red-500 hover:underline">
+    {text}
+  </a>
+);
 
-    principal: <>
-      {wikiLink("Oliver Oakes", "https://en.wikipedia.org/wiki/Oliver_Oakes")}
-    </>,
+export default {
+  base: <>
+    {wikiLink("Silverstone, United Kingdom", "https://en.wikipedia.org/wiki/Silverstone")},
+  </>,
 
-    technicalDirector: <>
-      {wikiLink("David Sanchez", "https://en.wikipedia.org/wiki/David_Sanchez_(engineer)")}
-    </>,
+  principal: <>
+    {wikiLink("Andy Cowell", "https://en.wikipedia.org/wiki/Andy_Cowell")}
+  </>,
 
-    website: "https://www.alpinef1.com/",
+  technicalDirector: <>
+    {wikiLink("Enrico Cardile", "https://en.wikipedia.org/wiki/Enrico_Cardile")}
+  </>,
 
-    raceDrivers: [
-      { id: "gasly", name: "Pierre Gasly", nationality: "France", number: 10 },
-      { id: "doohan", name: "Jack Doohan", nationality: "Australia", number: 7 }
-    ],
-    
-    testDrivers: [
-      { name: wikiLink("Franco Colapinto", "https://en.wikipedia.org/wiki/Franco_Colapinto"), nationality: "Argentina" },
-      { name: wikiLink("Paul Aron", "https://en.wikipedia.org/wiki/Paul_Aron"), nationality: "Estonia" },
-      { name: wikiLink("Kush Maini", "https://en.wikipedia.org/wiki/Kush_Maini"), nationality: "India" },
-      { name: wikiLink("Ryō Hirakawa", "https://en.wikipedia.org/wiki/Ry%C5%8D_Hirakawa"), nationality: "Japan" }
-    ],    
+  website: "https://www.astonmartinf1.com/",
 
-    chassis: wikiLink("A525", "https://en.wikipedia.org/wiki/Alpine_A525"),
-    engine: wikiLink("Renault", "https://en.wikipedia.org/wiki/Renault_in_Formula_One"),
-    tyres: wikiLink("Pirelli", "https://en.wikipedia.org/wiki/Pirelli"),
+  raceDrivers: [
+    { id: "alonso", name: "Fernando Alonso", nationality: "Spain", number: 14 },
+    { id: "stroll", name: "Lance Stroll", nationality: "Canada", number: 18 }
+  ],
 
-    careerStats: {
-      firstRace: wikiLink("2021 Bahrain Grand Prix", "https://en.wikipedia.org/wiki/2021_Bahrain_Grand_Prix"),
-      racesEntered: 95,
-      engines: wikiLink("Renault", "https://en.wikipedia.org/wiki/Renault_in_Formula_One"),
-      constructorsChampionships: 0,
-      driversChampionships: 0,
-      raceVictories: 1,
-      podiums: 6,
-      points: 519,
-      poles: 0,
-      fastestLaps: 1
-    }
-  };
-  
+  testDrivers: [
+    { name: wikiLink("Felipe Drugovich", "https://en.wikipedia.org/wiki/Felipe_Drugovich"), nationality: "Brazil" },
+    { name: wikiLink("Jak Crawford", "https://en.wikipedia.org/wiki/Jak_Crawford"), nationality: "United States" },
+    { name: wikiLink("Stoffel Vandoorne", "https://en.wikipedia.org/wiki/Stoffel_Vandoorne"), nationality: "Belgium" }
+  ],
+
+  chassis: wikiLink("AMR25", "https://en.wikipedia.org/wiki/Aston_Martin_AMR25"),
+  engine: wikiLink("Mercedes", "https://en.wikipedia.org/wiki/Mercedes_AMG_High_Performance_Powertrains"),
+  tyres: wikiLink("Pirelli", "https://en.wikipedia.org/wiki/Pirelli"),
+
+  careerStats: {
+    firstRace: wikiLink("1959 Dutch Grand Prix", "https://en.wikipedia.org/wiki/1959_Dutch_Grand_Prix"),
+    racesEntered: 101,
+    engines: <>Aston Martin, {wikiLink("Mercedes", "https://en.wikipedia.org/wiki/Mercedes_AMG_High_Performance_Powertrains")}</>,
+    constructorsChampionships: 0,
+    driversChampionships: 0,
+    raceVictories: 0,
+    podiums: 9,
+    points: 516,
+    poles: 0,
+    fastestLaps: 3
+  }
+};
