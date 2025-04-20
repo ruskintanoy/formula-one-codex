bdf5bb245c8322ce6701b6f820655e5e8b9125e3
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-04-20 01:24:50 -0600

    red bull stats done

--- data/teams/red_bull/info.js
+++ data/teams/red_bull/info.js
@@ -1,51 +1,55 @@
 const wikiLink = (text, url) => (
-    <a href={url} target="_blank" rel="noopener noreferrer" className="text-red-500 hover:underline">
-      {text}
-    </a>
-  );
-  
-  export default {
-    base: <>
-      {wikiLink("Enstone, United Kingdom", "https://en.wikipedia.org/wiki/Enstone")}
+  <a href={url} target="_blank" rel="noopener noreferrer" className="text-red-500 hover:underline">
+    {text}
+  </a>
+);
+
+export default {
+  base: <>
+    {wikiLink("Milton Keynes, United Kingdom", "https://en.wikipedia.org/wiki/Milton_Keynes")}
+  </>,
+
+  principal: <>
+    {wikiLink("Christian Horner", "https://en.wikipedia.org/wiki/Christian_Horner")}
+  </>,
+
+  technicalDirector: <>
+    {wikiLink("Pierre Waché", "https://en.wikipedia.org/wiki/Pierre_Wach%C3%A9")}
+  </>,
+
+  website: "https://redbullracing.com",
+
+  raceDrivers: [
+    { id: "verstappen", name: "Max Verstappen", nationality: "Netherlands",},
+    { id: "tsunoda", name: "Yuki Tsunoda", nationality: "Japan",},
+    { id: "lawson", name: "Liam Lawson", nationality: "New Zealander",}
+  ],
+
+  testDrivers: [
+    { name: wikiLink("Ayumu Iwasa", "https://en.wikipedia.org/wiki/Ayumu_Iwasa"), nationality: "Japan" }
+  ],
+
+  chassis: wikiLink("RB21", "https://en.wikipedia.org/wiki/Red_Bull_Racing_RB21"),
+  engine: wikiLink("Honda RBPT", "https://en.wikipedia.org/wiki/Honda_RBPT"),
+  tyres: wikiLink("Pirelli", "https://en.wikipedia.org/wiki/Pirelli"),
+
+  careerStats: {
+    firstRace: wikiLink("2005 Australian Grand Prix", "https://en.wikipedia.org/wiki/2005_Australian_Grand_Prix"),
+    racesEntered: 399,
+    engines: <>
+      {wikiLink("Cosworth", "https://en.wikipedia.org/wiki/Cosworth")},{" "}
+      {wikiLink("Ferrari", "https://en.wikipedia.org/wiki/Scuderia_Ferrari")},{" "}
+      {wikiLink("Renault", "https://en.wikipedia.org/wiki/Renault_in_Formula_One")},{" "}
+      {wikiLink("TAG Heuer", "https://en.wikipedia.org/wiki/TAG_Heuer")},{" "}
+      {wikiLink("Honda", "https://en.wikipedia.org/wiki/Honda_in_Formula_One")},{" "}
+      {wikiLink("RBPT", "https://en.wikipedia.org/wiki/Red_Bull_Powertrains")}
     </>,
-
-    principal: <>
-      {wikiLink("Oliver Oakes", "https://en.wikipedia.org/wiki/Oliver_Oakes")}
-    </>,
-
-    technicalDirector: <>
-      {wikiLink("David Sanchez", "https://en.wikipedia.org/wiki/David_Sanchez_(engineer)")}
-    </>,
-
-    website: "https://www.alpinef1.com/",
-
-    raceDrivers: [
-      { id: "gasly", name: "Pierre Gasly", nationality: "France", number: 10 },
-      { id: "doohan", name: "Jack Doohan", nationality: "Australia", number: 7 }
-    ],
-    
-    testDrivers: [
-      { name: wikiLink("Franco Colapinto", "https://en.wikipedia.org/wiki/Franco_Colapinto"), nationality: "Argentina" },
-      { name: wikiLink("Paul Aron", "https://en.wikipedia.org/wiki/Paul_Aron"), nationality: "Estonia" },
-      { name: wikiLink("Kush Maini", "https://en.wikipedia.org/wiki/Kush_Maini"), nationality: "India" },
-      { name: wikiLink("Ryō Hirakawa", "https://en.wikipedia.org/wiki/Ry%C5%8D_Hirakawa"), nationality: "Japan" }
-    ],    
-
-    chassis: wikiLink("A525", "https://en.wikipedia.org/wiki/Alpine_A525"),
-    engine: wikiLink("Renault", "https://en.wikipedia.org/wiki/Renault_in_Formula_One"),
-    tyres: wikiLink("Pirelli", "https://en.wikipedia.org/wiki/Pirelli"),
-
-    careerStats: {
-      firstRace: wikiLink("2021 Bahrain Grand Prix", "https://en.wikipedia.org/wiki/2021_Bahrain_Grand_Prix"),
-      racesEntered: 95,
-      engines: wikiLink("Renault", "https://en.wikipedia.org/wiki/Renault_in_Formula_One"),
-      constructorsChampionships: 0,
-      driversChampionships: 0,
-      raceVictories: 1,
-      podiums: 6,
-      points: 519,
-      poles: 0,
-      fastestLaps: 1
-    }
-  };
-  
+    constructorsChampionships: 6,
+    driversChampionships: 8,
+    raceVictories: 123,
+    podiums: 284,
+    points: 7908,
+    poles: 105,
+    fastestLaps: 99
+  }
+};
