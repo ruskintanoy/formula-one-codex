550c1a6209ae39612b9c1bc8583c8f7c309d1f2d
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-04-20 01:09:13 -0600

    mclaren stats done

--- data/teams/mclaren/info.js
+++ data/teams/mclaren/info.js
@@ -1,51 +1,60 @@
 const wikiLink = (text, url) => (
-    <a href={url} target="_blank" rel="noopener noreferrer" className="text-red-500 hover:underline">
-      {text}
-    </a>
-  );
-  
-  export default {
-    base: <>
-      {wikiLink("Enstone, United Kingdom", "https://en.wikipedia.org/wiki/Enstone")}
+  <a href={url} target="_blank" rel="noopener noreferrer" className="text-red-500 hover:underline">
+    {text}
+  </a>
+);
+
+export default {
+  base: <>
+    {wikiLink("Woking, United Kingdom", "https://en.wikipedia.org/wiki/Woking")}
+  </>,
+
+  principal: <>
+    {wikiLink("Andrea Stella", "https://en.wikipedia.org/wiki/Andrea_Stella_(engineer)")}
+  </>,
+
+  technicalDirector: <>
+    Neil Houldey and Peter Prodromou
+  </>,
+
+  founder: wikiLink("Bruce McLaren", "https://en.wikipedia.org/wiki/Bruce_McLaren"),
+
+  website: "https://www.mclaren.com/racing/formula-1/",
+
+  raceDrivers: [
+    { id: "norris", name: "Lando Norris", nationality: "Great Britain"},
+    { id: "piastri", name: "Oscar Piastri", nationality: "Australia"}
+  ],
+
+  testDrivers: [
+    { name: wikiLink("Valtteri Bottas", "https://en.wikipedia.org/wiki/Valtteri_Bottas"), nationality: "Finland" },
+    { name: wikiLink("Pato O'Ward", "https://en.wikipedia.org/wiki/Pato_O%27Ward"), nationality: "Mexico" }
+  ],
+
+  chassis: wikiLink("MCL39", "https://en.wikipedia.org/wiki/McLaren_MCL39"),
+  engine: wikiLink("Mercedes", "https://en.wikipedia.org/wiki/Mercedes_AMG_High_Performance_Powertrains"),
+  tyres: wikiLink("Pirelli", "https://en.wikipedia.org/wiki/Pirelli"),
+
+  careerStats: {
+    firstRace: wikiLink("1966 Monaco Grand Prix", "https://en.wikipedia.org/wiki/1966_Monaco_Grand_Prix"),
+    racesEntered: 979,
+    engines: <>
+      {wikiLink("Ford", "https://en.wikipedia.org/wiki/Ford_Motor_Company")}, {" "}
+      {wikiLink("Serenissima", "https://en.wikipedia.org/wiki/Scuderia_Serenissima")}, {" "}
+      {wikiLink("BRM", "https://en.wikipedia.org/wiki/British_Racing_Motors")}, {" "}
+      {wikiLink("Alfa Romeo", "https://en.wikipedia.org/wiki/Alfa_Romeo_in_Formula_One")}, {" "}
+      {wikiLink("TAG", "https://en.wikipedia.org/wiki/Techniques_d%27Avant_Garde")}, {" "}
+      {wikiLink("Honda", "https://en.wikipedia.org/wiki/Honda_in_Formula_One")}, {" "}
+      {wikiLink("Peugeot", "https://en.wikipedia.org/wiki/Peugeot")}, {" "}
+      {wikiLink("Mercedes", "https://en.wikipedia.org/wiki/Mercedes_AMG_High_Performance_Powertrains")}, {" "}
+      {wikiLink("Renault", "https://en.wikipedia.org/wiki/Renault_in_Formula_One")}
     </>,
-
-    principal: <>
-      {wikiLink("Oliver Oakes", "https://en.wikipedia.org/wiki/Oliver_Oakes")}
-    </>,
-
-    technicalDirector: <>
-      {wikiLink("David Sanchez", "https://en.wikipedia.org/wiki/David_Sanchez_(engineer)")}
-    </>,
-
-    website: "https://www.alpinef1.com/",
-
-    raceDrivers: [
-      { id: "gasly", name: "Pierre Gasly", nationality: "France", number: 10 },
-      { id: "doohan", name: "Jack Doohan", nationality: "Australia", number: 7 }
-    ],
-    
-    testDrivers: [
-      { name: wikiLink("Franco Colapinto", "https://en.wikipedia.org/wiki/Franco_Colapinto"), nationality: "Argentina" },
-      { name: wikiLink("Paul Aron", "https://en.wikipedia.org/wiki/Paul_Aron"), nationality: "Estonia" },
-      { name: wikiLink("Kush Maini", "https://en.wikipedia.org/wiki/Kush_Maini"), nationality: "India" },
-      { name: wikiLink("Ryō Hirakawa", "https://en.wikipedia.org/wiki/Ry%C5%8D_Hirakawa"), nationality: "Japan" }
-    ],    
-
-    chassis: wikiLink("A525", "https://en.wikipedia.org/wiki/Alpine_A525"),
-    engine: wikiLink("Renault", "https://en.wikipedia.org/wiki/Renault_in_Formula_One"),
-    tyres: wikiLink("Pirelli", "https://en.wikipedia.org/wiki/Pirelli"),
-
-    careerStats: {
-      firstRace: wikiLink("2021 Bahrain Grand Prix", "https://en.wikipedia.org/wiki/2021_Bahrain_Grand_Prix"),
-      racesEntered: 95,
-      engines: wikiLink("Renault", "https://en.wikipedia.org/wiki/Renault_in_Formula_One"),
-      constructorsChampionships: 0,
-      driversChampionships: 0,
-      raceVictories: 1,
-      podiums: 6,
-      points: 519,
-      poles: 0,
-      fastestLaps: 1
-    }
-  };
-  
+    constructorsChampionships: 9,
+    driversChampionships: 12,
+    raceVictories: 192,
+    podiums: 531,
+    points: 7108.5,
+    poles: 167,
+    fastestLaps: 174
+  }
+};
